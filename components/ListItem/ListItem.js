afd2344d3202e04603c70f768c747b0a5808fc21
dimas-id <dimas.ilham01@ui.ac.id>
2023-09-28 16:57:32 +0700

    FIX: fix cursor pointer

--- components/ListItem/ListItem.js
+++ components/ListItem/ListItem.js
@@ -6,7 +6,8 @@ export default function ListItem({title, time, onClick}) {
     return (
         <div onClick={() => onClick() } 
             className="flex flex-col items-start self-stretch py-3 px-8
-                            bg-white-200 border-solid border-b-2 border-black-950">
+                            bg-white-200 border-solid border-b-2 border-black-950
+                            hover:cursor-pointer">
             <h1 className="font-body leading-6 text-xl font-semibold text-black">{title}</h1>
             <div className="flex flex-row gap-5">
                 <p className="font-body text-md text-black">Dibuat pada: {formattedDate}</p>
--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -8,17 +8,17 @@ export default function Sidebar() {
     return (
         <div className="flex flex-col min-h-screen w-1/5 items-center py-24 px-8 bg-blue-900">
             <div className="flex p-8" onClick={() => router.push('/overview')}>
-                <h1 className="font-heading place-self-center text-4xl font-bold text-white">AINGS</h1>
+                <h1 className="font-heading place-self-center text-4xl font-bold text-white hover:cursor-pointer">AINGS</h1>
             </div>
             <div className="flex flex-col w-48 place-items-center self-stretch">
-                <div className="self-stretch hover:bg-blue-950" onClick={() => router.push('/overview')}>
-                    <button className="font-body text-lg py-2 px-3 self-stretch text-white">Overview</button>
+                <div className="self-stretch hover:bg-blue-950 hover:cursor-pointer" onClick={() => router.push('/overview')}>
+                    <p className="font-body text-lg py-2 px-3 self-stretch text-white ">Overview</p>
                 </div>
-                <div className="self-stretch hover:bg-blue-950" onClick={() => router.push('/draf')}>
-                    <button className="font-body text-lg py-2 px-3 self-stretch text-white">Daftar Draf Berita</button>
+                <div className="self-stretch hover:bg-blue-950 hover:cursor-pointer" onClick={() => router.push('/draf')}>
+                    <p className="font-body text-lg py-2 px-3 self-stretch text-white">Daftar Draf Berita</p>
                 </div>
-                <div className="self-stretch hover:bg-blue-950" onClick={() => router.push('/draf/create')}>
-                    <button className="font-body text-lg py-2 px-3 self-stretch text-white">Buat Draf Berita</button>
+                <div className="self-stretch hover:bg-blue-950 hover:cursor-pointer" onClick={() => router.push('/draf/create')}>
+                    <p className="font-body text-lg py-2 px-3 self-stretch text-white">Buat Draf Berita</p>
                 </div>
             </div>
         </div>
