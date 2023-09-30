859ce25508508fcf0e0da2a9ed25a196f6bbea78
dimas-id <dimas.ilham01@ui.ac.id>
2023-09-30 20:50:10 +0700

    FIX: fix edit layout

--- components/Draf/DraftEditLayout.js
+++ components/Draf/DraftEditLayout.js
@@ -1,11 +1,17 @@
 import Editor from "../Editor/Editor";
 
 export default function DraftEditLayout({ title, content }) {
+
+    const placeholder = "ini content";
+
     return (
-        <main>
-            <div className="flex flex-col bg-blue-50">
+        <main className="bg-blue-50 min-h-screen">
+            <div className="flex flex-col py-24 px-20">
                 <h1 className="font-heading text-4xl">{title}</h1>
-                <Editor contents={content} />
+                <Editor 
+                    className="flex flex-col h-[480px] py-4 px-4 items-start self-stretch 
+                    bg-white border-2 border-blue-400 rounded-md gap-y-2 overflow-y-auto"
+                    contents={content} />
             </div>
         </main>
     );
