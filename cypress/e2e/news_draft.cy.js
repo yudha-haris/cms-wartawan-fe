2818781d8013da9e49012566bd28fea3848ac8a4
Yudha Haris Purnama <yudha@clade.ventures>
2024-01-06 09:30:59 +0700

    test: news draft

--- cypress/e2e/news_draft.cy.js
+++ cypress/e2e/news_draft.cy.js
@@ -47,25 +47,4 @@ describe("Authentication spec", () => {
     // verify
     cy.get("p").contains("2/").should("be.visible");
   });
-
-  // it("User can logout", () => {
-  //   // interceptor
-  //   cy.intercept("https://ta-aings-399219.uc.r.appspot.com/v1/draft?*").as(
-  //     "getDraft"
-  //   );
-  //   // steps
-  //   cy.get('input[placeholder="Username"').type("budi.w");
-  //   cy.get('input[placeholder="Password"').type("budi123");
-  //   cy.get("button")
-  //     .contains(/^Masuk$/)
-  //     .click();
-  //   cy.get("p").contains("Daftar Draf Berita").should("be.visible");
-
-  //   cy.wait("@getDraft");
-
-  //   cy.get("p").filter(":visible").contains("Keluar").click();
-
-  //   // verify
-  //   cy.get('input[placeholder="Username"').should("be.visible");
-  // });
 });
