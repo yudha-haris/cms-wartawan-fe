4d4d4c3d6850c0d6d15c2fcedf2f2433588473a0
Yudha Haris Purnama <yudha@clade.ventures>
2024-01-06 09:30:43 +0700

    test: news draft

---
+++ cypress/e2e/news_draft.cy.js
@@ -0,0 +1,71 @@
+/**
+ * - News Draft spec
+ *   - User can access News Draft page
+ *   - User can navigate through pages in News Draft page
+ */
+
+describe("Authentication spec", () => {
+  beforeEach("Visit Page", () => {
+    cy.visit("http://localhost:3000/auth/login?");
+  });
+
+  it("User can access News Draft page", () => {
+    // interceptor
+    cy.intercept("https://ta-aings-399219.uc.r.appspot.com/v1/draft?*").as(
+      "getDraft"
+    );
+
+    // steps
+    cy.get('input[placeholder="Username"').type("budi.w");
+    cy.get('input[placeholder="Password"').type("budi123");
+    cy.get("button")
+      .contains(/^Masuk$/)
+      .click();
+
+    cy.wait("@getDraft");
+
+    // verify
+    cy.get("p").contains("Daftar Draf Berita").should("be.visible");
+  });
+
+  it("User can navigate through pages in News Draft page", () => {
+    // interceptor
+    cy.intercept("https://ta-aings-399219.uc.r.appspot.com/v1/draft?*").as(
+      "getDraft"
+    );
+
+    // steps
+    cy.get('input[placeholder="Username"').type("budi.w");
+    cy.get('input[placeholder="Password"').type("budi123");
+    cy.get("button")
+      .contains(/^Masuk$/)
+      .click();
+
+    cy.wait("@getDraft");
+    cy.get("p").contains("Berikutnya").click();
+
+    // verify
+    cy.get("p").contains("2/").should("be.visible");
+  });
+
+  // it("User can logout", () => {
+  //   // interceptor
+  //   cy.intercept("https://ta-aings-399219.uc.r.appspot.com/v1/draft?*").as(
+  //     "getDraft"
+  //   );
+  //   // steps
+  //   cy.get('input[placeholder="Username"').type("budi.w");
+  //   cy.get('input[placeholder="Password"').type("budi123");
+  //   cy.get("button")
+  //     .contains(/^Masuk$/)
+  //     .click();
+  //   cy.get("p").contains("Daftar Draf Berita").should("be.visible");
+
+  //   cy.wait("@getDraft");
+
+  //   cy.get("p").filter(":visible").contains("Keluar").click();
+
+  //   // verify
+  //   cy.get('input[placeholder="Username"').should("be.visible");
+  // });
+});
